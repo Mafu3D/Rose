6e75ae02d52930fecbef5fd7a4c61bd03fe9a122
MAFUPC\mafu <matthewfuller3d@gmail.com>
2025-10-19 15:50:30 -0700

    fix to scratch cheats

--- Assets/Scripts/_Scratch/Scratch.cs
+++ Assets/Scripts/_Scratch/Scratch.cs
@@ -27,22 +27,22 @@ public class Scratch : MonoBehaviour
         }
 
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.C))
         {
             Player.GoldTracker.AddGold(1);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.V))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.V))
         {
             Player.GoldTracker.RemoveGold(1);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.G))
         {
             Player.GemTracker.AddGem(1);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.H))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.H))
         {
             Player.GemTracker.RemoveGem(1);
         }
